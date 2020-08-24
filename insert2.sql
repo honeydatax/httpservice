5fac5498de1de33f8c86da9aab97846314977571
lisbon <16432726+honeydatax@users.noreply.github.com>
2020-08-24 19:51:56 +0100

    Delete insert2.sql

--- insert2.sql
+++
@@ -1,7 +0,0 @@
-insert into clients (ID,NAME,ADDRESS,PHONE)
-values (0,"man","californa","999999");
-insert into clients (ID,NAME,ADDRESS,PHONE)
-values (2,"jim","californa","999999");
-insert into clients (ID,NAME,ADDRESS,PHONE)
-values (3,"bill","californa","999999");
-.quit
